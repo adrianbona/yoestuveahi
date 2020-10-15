d597f4c8698f48b96c2aacc93924646574448a3b
Adrian Bona <adrian.bona@clevertech.biz>
2020-10-15 00:46:23 -0300

    Prettier

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Link = ({ active, children, onClick }) => (
   <button
@@ -11,12 +11,12 @@ const Link = ({ active, children, onClick }) => (
   >
     {children}
   </button>
-)
+);
 
 Link.propTypes = {
   active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired
-}
+};
 
-export default Link
+export default Link;
