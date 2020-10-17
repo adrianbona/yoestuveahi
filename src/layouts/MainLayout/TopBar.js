a8cf71e9a494cebbfc136f8e9d3711dd201a1528
Adrian Bona <adrian.bona@clevertech.biz>
2020-10-17 15:10:34 -0300

    no message

--- src/layouts/MainLayout/TopBar.js
+++ src/layouts/MainLayout/TopBar.js
@@ -2,32 +2,36 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import {
-  AppBar,
-  Toolbar,
-  makeStyles
-} from '@material-ui/core';
+import { AppBar, Toolbar, makeStyles, Typography } from '@material-ui/core';
 import Logo from 'src/components/Logo';
 
-const useStyles = makeStyles(({
+const useStyles = makeStyles({
   root: {},
   toolbar: {
     height: 64
+  },
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    '& > h3': {
+      marginLeft: '24px'
+    }
   }
-}));
+});
 
 const TopBar = ({ className, ...rest }) => {
   const classes = useStyles();
 
   return (
-    <AppBar
-      className={clsx(classes.root, className)}
-      elevation={0}
-      {...rest}
-    >
+    <AppBar className={clsx(classes.root, className)} elevation={0} {...rest}>
       <Toolbar className={classes.toolbar}>
         <RouterLink to="/">
-          <Logo />
+          <div className={classes.title}>
+            <Logo />
+            <Typography align="center" color="textPrimary" variant="h3">
+              Yo Estuve Ahí App
+            </Typography>
+          </div>
         </RouterLink>
       </Toolbar>
     </AppBar>
