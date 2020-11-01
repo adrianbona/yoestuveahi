576886f1ed0e6dafdc46efd74a2f79df3f279b98
Adrian Bona <adrian.bona@clevertech.biz>
2020-11-01 19:48:57 -0300

    Swtich photos coming from Google

--- src/components/location/AddLocationModal.js
+++ src/components/location/AddLocationModal.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Avatar, Box, Card, CardContent, makeStyles } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  makeStyles
+} from '@material-ui/core';
 import { getLatLng } from 'react-places-autocomplete';
 import SimpleModal from '../SimpleModal';
 import LocationSearchInput from '../LocationSearchInput';
@@ -15,18 +22,19 @@ const useStyles = makeStyles(() => ({
 const AddLocationModal = props => {
   const { onClose, open, customer } = props;
   const [selectedAddress, setSelectedAddress] = useState(null);
-  const [selectedPlacePhoto, setSelectedPlacePhoto] = useState(null);
+  const [placePhotos, setPlacePhotos] = useState([]);
+  const [photoIndex, setPhotoIndex] = useState(0);
   const classes = useStyles();
 
   const handleClose = () => {
-    setSelectedPlacePhoto(null);
+    setPlacePhotos([]);
     onClose();
   };
 
   const getPictures = async address => {
     try {
       const { data } = await getPhotos(address.place_id);
-      setSelectedPlacePhoto(data.result.photos[0].photo_reference);
+      setPlacePhotos(data.result.photos);
     } catch (err) {}
     getLatLng(address).then(latLng => latLng);
   };
@@ -47,15 +55,41 @@ const AddLocationModal = props => {
               onSelectedAddress={address => setSelectedAddress(address)}
             />
           </Box>
-          {selectedPlacePhoto && (
-            <Box alignItems="center" display="flex" flexDirection="column">
-              <Avatar
-                className={classes.fullWidth}
-                alt="Place"
-                src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${selectedPlacePhoto}&key=AIzaSyDAvfXiXVeMN803sCuPUexOzkVy-dgnqdE`}
-                variant="rounded"
-              />
-            </Box>
+          {placePhotos.length > 0 && (
+            <>
+              <Box alignItems="center" display="flex" flexDirection="column">
+                <Avatar
+                  className={classes.fullWidth}
+                  alt="Place"
+                  src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${placePhotos[photoIndex].photo_reference}&key=AIzaSyDAvfXiXVeMN803sCuPUexOzkVy-dgnqdE`}
+                  variant="rounded"
+                />
+              </Box>
+              <Box
+                p={2}
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-evenly"
+              >
+                <Button
+                  className={classes.scanAgain}
+                  color="secondary"
+                  variant="contained"
+                  onClick={() =>
+                    setPhotoIndex((photoIndex + 1) % placePhotos.length)
+                  }
+                >
+                  Change image
+                </Button>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={handleClose}
+                >
+                  Create
+                </Button>
+              </Box>
+            </>
           )}
         </CardContent>
       </Card>
