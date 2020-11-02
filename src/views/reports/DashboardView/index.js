0e5dcff46d587db7cc8c992aa6f2090672fe48db
Adrian Bona <adrian.bona@clevertech.biz>
2020-11-01 21:07:28 -0300

    More clean up

--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Container, Grid, makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Budget from './Budget';
-import LatestOrders from './LatestOrders';
-import LatestProducts from './LatestProducts';
 import Sales from './Sales';
 import TasksProgress from './TasksProgress';
 import TotalCustomers from './TotalCustomers';
@@ -26,8 +24,11 @@ const Dashboard = () => {
     <Page className={classes.root} title="Dashboard">
       <Container maxWidth={false}>
         <Grid container spacing={3}>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <LatestOrders />
+          <Grid item sm={12} md={6} lg={6} xl={6} xs={12}>
+            <Sales />
+          </Grid>
+          <Grid item sm={12} md={6} lg={6} xl={6} xs={12}>
+            <TrafficByDevice />
           </Grid>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <Budget />
@@ -40,15 +41,6 @@ const Dashboard = () => {
           </Grid>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <TotalProfit />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <Sales />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <TrafficByDevice />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <LatestProducts />
           </Grid>
         </Grid>
       </Container>
