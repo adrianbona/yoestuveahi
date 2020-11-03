2943456827b8525bccfc905969f6529580a0bb20
Adrian Bona <adrian.bona@clevertech.biz>
2020-11-03 11:55:12 -0300

    Remove body from post

--- src/redux/api/authentication.js
+++ src/redux/api/authentication.js
@@ -2,10 +2,8 @@ import { instance } from './axios';
 
 export function login({ email, password }) {
   return instance.post('/login', {
-    body: {
-      email,
-      password
-    }
+    username: email,
+    password
   });
 }
 
