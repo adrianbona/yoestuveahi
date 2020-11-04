71b6e231e8d78a9dc026b6ebc91c8e3e83912c34
Adrian Bona <adrian.bona@clevertech.biz>
2020-11-04 20:16:38 -0300

    Probá con esta

--- src/redux/api/axios.js
+++ src/redux/api/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: 'http://localhost:8000',
   headers: {
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
   }
 });
