f6add31f882d40f2200f9fe888b2b04591b4438b
Adrian Bona <adrian.bona@clevertech.biz>
2020-11-19 12:53:03 -0300

    From Google Maps to OpenMaps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,66 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
 import PropTypes from 'prop-types';
-import Marker from './Marker';
+import {
+  MapContainer as LeafletMap,
+  TileLayer,
+  Marker as LeafletMarker,
+  Popup
+} from 'react-leaflet';
+import { makeStyles } from '@material-ui/styles';
+import theme from '../theme';
+
+const useStyles = makeStyles({
+  marker: {
+    width: 'max-content',
+    color: theme.palette.secondary.light,
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+    fontWeight: 'bolder',
+    fontSize: 'larger'
+  },
+  capacity: {
+    width: 'max-content',
+    color: theme.palette.primary.light,
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+    fontWeight: 'bolder',
+    fontSize: 'larger',
+    paddingBottom: '10px'
+  }
+});
 
 const SimpleMap = ({ markers }) => {
-  const defaultProps = {
-    center: {
-      lat: -34.58,
-      lng: -58.44
-    },
-    zoom: 13
-  };
-
+  const classes = useStyles();
   return (
     <div style={{ height: '40vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY_GOOGLE }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+      <LeafletMap
+        style={{ height: '100%', width: '100%' }}
+        center={[-34.58, -58.44]}
+        zoom={13}
+        scrollWheelZoom
       >
-        {markers.map(marker => (
-          <Marker
-            key={marker.id}
-            lat={marker.latitude}
-            lng={marker.longitude}
-            location={marker}
-          />
-        ))}
-      </GoogleMapReact>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map(
+          ({
+            id,
+            latitude,
+            longitude,
+            name,
+            currentCapacity,
+            maximumCapacity
+          }) => (
+            <LeafletMarker key={id} position={[latitude, longitude]}>
+              <Popup>
+                <div className={classes.marker}>{name}</div>
+                <div className={classes.capacity}>
+                  {`${maximumCapacity - currentCapacity} /  ${maximumCapacity}`}
+                </div>
+              </Popup>
+            </LeafletMarker>
+          )
+        )}
+      </LeafletMap>
     </div>
   );
 };
